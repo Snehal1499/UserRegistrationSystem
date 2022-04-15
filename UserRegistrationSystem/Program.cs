4b4673f14c9749189aa2f8349a7c783e383c9fee
Snehal1499 <snehalpatil1718@gmail.com>
2022-04-15 22:53:48 +0530

    Added UC1 First Name

--- UserRegistrationSystem/Program.cs
+++ UserRegistrationSystem/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
-namespace UserRegistrationSystem
+namespace UserRegistrationDay26
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To User Registration System");
+            Console.WriteLine("UC 1Enter First Name");
+            string firstName = Console.ReadLine();
+            Program p = new Program();
+
+            p.isMatchFirstName(firstName);
+
         }
+        //UC 1 First Name
+        public string isMatchFirstName(string firstName)
+        {
+            try
+            {
+                if (Regex.Match(firstName, "^[A-Z][a-z]{3,}$").Success)
+                {
+                    Console.WriteLine("Success , Firt Name is : " + firstName);
+                }
+                else
+                {
+                    Console.WriteLine("Failed");
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("First Name should Start with Upper Case Letter and minimum charecters should be 3 ");
+            }
+            return firstName;
+
+        }
+
     }
+
 }
